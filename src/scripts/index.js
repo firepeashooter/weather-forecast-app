60d710f0d469cb432c1fd5d0a893f3f7c5057fa0
Ben Braun <benjamin.braun2003@gmail.com>
2025-11-04 12:56:53 -0500

    Add form submission functionality

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,20 @@ const location = "kingstonontario"
 
 const data = await getWeatherData(location);
 console.log(data);
+
+
+const weatherForm = document.querySelector("#city--search");
+
+
+weatherForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const inputField = document.querySelector("#city--input");
+    const inputValue = inputField.value;
+
+    //Error handeling/check if the input string is valid > 2 and maybe if it doesn't go through we send
+    //the user an error to try again
+
+    alert(inputValue);
+})

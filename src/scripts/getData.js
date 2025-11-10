7695f2fc94822e842fb5623f3d631435ade72273
Ben Braun <benjamin.braun2003@gmail.com>
2025-11-10 15:46:11 -0500

    Add proper formatting to weatherCard

--- src/scripts/getData.js
+++ src/scripts/getData.js
@@ -1,44 +1,53 @@
 import { formatWeatherData } from "./formatData";
 
-export async function getWeatherData(location){
+export async function getWeatherData(location) {
 
-    let vcKey = "YNK7LTYXNBZKEY3NKXCKEYN54"
+	let vcKey = "YNK7LTYXNBZKEY3NKXCKEYN54"
 
-    let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location +"?key=" + vcKey;
+	let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "?key=" + vcKey;
 
-    try {
-        const response = await fetch(url);
-        const weatherData = await response.json();
-        const data = await formatWeatherData(weatherData);
-        return data;
+	try {
+		const response = await fetch(url);
+		const weatherData = await response.json();
+		const data = await formatWeatherData(weatherData);
+		return data;
 
-    } catch (error) {
-        alert("Error Retrieving data from API:  " + error);
-    }
- 
+	} catch (error) {
+		alert("Error Retrieving data from API:  " + error);
+	}
+
 
 }
 
 
-export function populateWeatherCard(data){
-    //this is where we update the data to the card
+export function populateWeatherCard(data) {
+	//this is where we update the data to the card
 
-    const locationHeader = document.querySelector("#location");
-    const conditionHeader = document.querySelector("#condition");
-    const tempHeader = document.querySelector("#temp");
-    const feelsHeader = document.querySelector("#feels--like");
-    const humidityHeader = document.querySelector("#humidity");
-    const windSpeedHeader = document.querySelector("#wind--speed");
+	const locationHeader = document.querySelector("#location");
+	const conditionHeader = document.querySelector("#condition");
+	const tempHeader = document.querySelector("#temp");
+	const feelsHeader = document.querySelector("#feels--like");
+	const humidityHeader = document.querySelector("#humidity");
+	const windSpeedHeader = document.querySelector("#wind--speed");
 
-    
-    //TODO: WE WANT TO FORMAT ALL OF THIS INFORMATION SO IT'S MORE PLEASING
-    conditionHeader.textContent = data.conditions;
-    locationHeader.textContent = data.location;
-    tempHeader.textContent = data.temp;
-    feelsHeader.textContent = "Feels Like: " + data.feelsLike;
-    humidityHeader.textContent = "Humidity: " + data.humidity;
-    windSpeedHeader.textContent = "Wind: " + data.windSpeed;
+
+
+	//TODO: WE WANT TO FORMAT ALL OF THIS INFORMATION SO IT'S MORE PLEASING
+	conditionHeader.textContent = "Current Conditions: " + data.conditions;
+	locationHeader.textContent = data.location;
+	tempHeader.innerHTML = farenheitToCelicius(data.temp) + " &deg;C";
+	feelsHeader.innerHTML = "Feels Like: " + farenheitToCelicius(data.feelsLike) + " &deg;C";
+	humidityHeader.textContent = "Humidity: " + data.humidity + "%";
+	windSpeedHeader.textContent = "Wind Speed: " + data.windSpeed + " km/h";
 }
 
 
-   
+function farenheitToCelicius(temp) {
+
+	const celc = (temp - 32) * (5 / 9);
+
+	return celc.toFixed(0);
+}
+
+
+
